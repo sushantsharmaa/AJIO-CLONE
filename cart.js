f2203f5e8d481a7713850d066932ba219fcbf66d
Himanshu <himanshubisht3112@gmail.com>
2021-07-25 19:39:02 +0530

    Updated

--- cart.js
+++ cart.js
@@ -4,18 +4,18 @@ let total = document.getElementById("total");
 
 let price1 = document.getElementById("bagPrice");
 let op = document.getElementById("opt");
-price.innerHTML = `Rs.1678`;
-discount.innerHTML = `Rs.500`;
-total.innerHTML = `Rs.1178`;
+price.innerHTML = `Rs.3050`;
+discount.innerHTML = `Rs.1372.5`;
+total.innerHTML = `Rs.1677.5`;
 op.onchange = function () {
   let x = op.value;
-  price1.innerHTML = `Rs.${x * 1678}`;
+  price1.innerHTML = `Rs.${x * 3050}`;
 
-  price.innerHTML = `Rs.${x * 1678}`;
+  price.innerHTML = `Rs.${x * 3050}`;
 
-  discount.innerHTML = `Rs.${x * 500}`;
-  let val = `${x * 1678}`;
-  let dis = `${x * 500}`;
+  discount.innerHTML = `Rs.${x * 1372.5}`;
+  let val = `${x * 3050}`;
+  let dis = `${x * 1372.5}`;
   total.innerHTML = `Rs.${val - dis}`;
 };
 
